d7420611b04a0f28724b39a862f72acc0902b62e
EveGatling <carolyarschel@gmail.com>
2020-04-03 19:22:56 -0300

    Login view continued

--- src/view/Login/Login.jsx
+++ src/view/Login/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
+import Rectangle from '../../Images/BlueRec.svg';
 import {useHistory} from "react-router-dom";
 
 import "./Login.css";
@@ -8,12 +9,13 @@ function LoginView(){
     return(
         <div className="All-login">
             <Header className="header-login">Notre Alunos</Header>
+            <img src={Rectangle} className="blue-rec" height="800"></img>
             <div className="login-box">
-                <p>Usuário</p>
-                <input></input>
-                <p>Senha</p>
-                <input></input>
+                <p className="title-login">Entrar</p>
+                <input placeholder="Email"></input>
+                <input placeholder="Senha" type="password"></input>
             </div>
+            <buttom className="btn-login">Entrar</buttom>
         </div>
     );
 }
